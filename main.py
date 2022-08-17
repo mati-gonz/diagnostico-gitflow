62bc829422c4620b2fe90375e174ead33deb31a9
MATIAS GONZALEZ <matigonz@uc.cl>
2022-08-17 00:08:51 -0400

    Creación archivo main

---
+++ main.py
@@ -0,0 +1,6 @@
+import top_dias_tweets, top_hashtags, top_retweets, top_tweets_usuarios
+
+print('Los top 10 tweets más retweeteados son: "función que no alcancé a hacer"')
+print('Los top 10 usuarios con más tweets son: "función que no alcancé a hacer"')
+print('Los top 10 días donde hay más tweets son: "función que no alcancé a hacer"')
+print('Los top 10 hashtags más usados son: "función que no alcancé a hacer"')
